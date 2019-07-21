62019066d38c85583677f984b5ff4c2e2dde5bfc
tabtelf <tabtelf@live.com>
2019-07-21 10:58:18 +0800

    02 Org-mode

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages (quote (org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,3 +35,10 @@
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+
+;; Org-mode stuff
+(use-package org-bullets
+  :ensure
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
